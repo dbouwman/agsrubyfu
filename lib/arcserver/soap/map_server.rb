96a33fcc46b9b09010646c77b4663d7ad65c449b
Dave Bouwman <dave@davebouwman.com>
2010-02-22 21:15:34 -0700

    Updates for SOAP request

--- lib/arcserver/soap/map_server.rb
+++ lib/arcserver/soap/map_server.rb
@@ -21,7 +21,65 @@ module ArcServer
         node = response.document.xpath('//tns:GetDefaultMapNameResponse/Result', ns).first
         parse_default_map_name_result(node)
       end
-
+      
+      def get_parcel_map(parcel_oid)
+        response = invoke("ns:ExportMapImage") do |message|
+          message.add "MapDescripion" do |mapdesc|
+            mapdesc.add "Name", 'Portland'
+            mapdesc.add "ns:MapArea" do |maparea|
+              maparea.add "Extent" do |extent|
+                extent.add "XMin", -13723300
+                extent.add "YMin", 5739100
+                extent.add "XMax", -13722300
+                extent.add "YMax", 5740000                  
+                end #extent
+              end #maparea
+            mapdesc.add "LayerDescriptions" do |layerdescs|
+              layerdescs.add "LayerDescription" do |layer0desc|
+                layer0desc.add "LayerId",0
+                layer0desc.add "Visible", "true"
+              end
+              layerdescs.add "LayerDescription" do |layer1desc|
+                layer1desc.add "LayerId",1
+                layer1desc.add "Visible", "true"
+                layer1desc.add "SelectionFeatures" do |selectionFeatures|
+                  selectionFeatures.add "Int", parcel_oid
+                end
+              end
+              
+              layerdescs.add "LayerDescription" do |layer2desc|
+                  layer2desc.add "LayerId",2
+                  layer2desc.add "Visible", "true"
+              end  
+              layerdescs.add "LayerDescription" do |layer3desc|
+                    layer3desc.add "LayerId",3
+                    layer3desc.add "Visible", "true"
+              end  
+              layerdescs.add "LayerDescription" do |layer4desc|
+                      layer4desc.add "LayerId",4
+                      layer4desc.add "Visible", "true"
+              end
+            end #end layerdescs
+            mapdesc.add "SpatialReference" do |spatialRef|
+              spatialRef.add "WKT", 'PROJCS["WGS_1984_Web_Mercator",GEOGCS["GCS_WGS_1984_Major_Auxiliary_Sphere",DATUM["D_WGS_1984_Major_Auxiliary_Sphere",SPHEROID["WGS_1984_Major_Auxiliary_Sphere",6378137.0,0.0]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Mercator"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",0.0],PARAMETER["Standard_Parallel_1",0.0],UNIT["Meter",1.0]]'
+            end
+          end #end mapdesc
+          message.add "ImageDescription" do |imgdesc|
+            imgdesc.add "ImageType" do |image_type|
+              image_type.add "ImageReturnType", image_return_type
+              image_type.add "ImageFormat", "esriImagePNG24"
+            end #image_type
+            imgdesc.add "ImageDisplay" do |image_display|
+              image_display.add "ImageHeight", 500
+              image_display.add "ImageWidth",500
+              image_display.add "ImageDPI",72
+            end #image_display
+          end #imgdesc
+        end #message
+        
+        #get the image url from the response
+        node = response.document.xpath('').first
+        
       def get_legend_info(args = {})
         image_return_type = args[:image_return_url] ? "esriImageReturnURL" : "esriImageReturnMimeData"
         response = invoke("ns:GetLegendInfo") do |message|
